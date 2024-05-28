e0c6c67976d1e4fb5c93837d67b388bf45078489
Adasat Torres de Leon <a.torres@binhex.cloud>
2024-05-28 12:25:17 +0100

    [ADD]account_statement_import_online_plaid

---
+++ account_statement_import_online_plaid/static/src/js/plaid_integration.esm.js
@@ -0,0 +1,23 @@
+/** @odoo-module **/
+/* global Plaid */
+import {registry} from "@web/core/registry";
+export async function plaid_login(env, action) {
+    const handler = Plaid.create({
+        clientName: "Plaid Quickstart",
+        product: ["transactions"],
+        onSuccess: (public_token) => {
+            console.log(env, action);
+            env.services.orm.call(
+                "online.bank.statement.provider",
+                "plaid_create_access_token",
+                [false, public_token, action.context.active_id]
+            );
+        },
+        countryCodes: ["US", "ES"],
+        language: "es",
+        token: action.args.token,
+    });
+    handler.open();
+}
+
+registry.category("actions").add("plaid_login", plaid_login);
